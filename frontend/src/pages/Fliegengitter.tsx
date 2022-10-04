fe68d1960458c3e54c5556c4bcda61e89d97a7d1
JB <JB@hardcore.com>
2022-10-04 18:31:41 +0200

    implemant basic inputs continued

--- frontend/src/pages/Fliegengitter.tsx
+++ frontend/src/pages/Fliegengitter.tsx
@@ -1,30 +1,55 @@
-import {Stack} from "@fluentui/react/lib/Stack";
+import {Stack, IStackTokens} from "@fluentui/react/lib/Stack";
 import {FormEvent, useState} from "react";
 import {TextField} from "@fluentui/react/lib/TextField";
 import {PrimaryButton} from "@fluentui/react/lib/Button";
+import {MessageBar} from "@fluentui/react";
 
 export function Fliegengitter() {
 
     const [length, setLength] = useState<number>();
+    const [width, setWidth] = useState<number>();
+    const [depth, setDepth] = useState<number>();
 
-    function setLengthAndConvertToNumber(e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) {
-        const value = Number(e.currentTarget.value);
-        if (isNaN(value)) {
-            alert("Ihr Wert ist nicht valide");
-        }
-        setLength(value);
+    interface IExampleProps {
+        resetChoice?: () => void;
+    }
+
+    const stacktoken : IStackTokens = {childrenGap: 10};
+
+    function setLengthAndConvertToNumber(e: FormEvent<HTMLInputElement|HTMLTextAreaElement>) {
+        const length = Number(e.currentTarget);
+        return setLength(length);
+    }
+
+    function setWidthAndConvertToNumber(e: FormEvent<HTMLInputElement|HTMLTextAreaElement>){
+        const width = Number(e.currentTarget);
+        return setWidth(width);
+    }
+
+    function setDepthAndConvertToNumber(e: FormEvent<HTMLInputElement|HTMLTextAreaElement>){
+        const depth = Number(e.currentTarget);
+        return setDepth(depth);
     }
 
     return <> <p>Fliegengitter</p>
-        <Stack horizontal>
-            <Stack.Item>
-                <TextField type="text" label="Länge" onChange={setLengthAndConvertToNumber}/>
+
+        <MessageBar>
+            Konfigurieren Sie Sich ihren individuellen Insektenschutz!!!
+        </MessageBar>
+            <Stack tokens={stacktoken}>
+            <Stack.Item align="baseline">
+                <TextField name="length" type="number" step="any" label="Länge" onChange={setLengthAndConvertToNumber}  placeholder="cm" required />
             </Stack.Item>
-        </Stack>
-        <Stack>
-            <Stack.Item>
-                <PrimaryButton text="Klick wen anders!" onClick={() => alert(length)}/>
+            <Stack.Item align="baseline">
+                <TextField name="width" type="number" step="any" label="Breite" onChange={setWidthAndConvertToNumber} placeholder="cm" required/>
             </Stack.Item>
-        </Stack>
+            <Stack.Item align="baseline">
+                <TextField name="depth" type="number" step="any" label="Tiefe" onChange={setDepthAndConvertToNumber} placeholder="cm" required/>
+            </Stack.Item>
+                <Stack.Item>
+                    <PrimaryButton text="Klick wen anders!" onClick={() => alert(length)}/>
+                </Stack.Item>
+            </Stack>
+
     </>
 }
